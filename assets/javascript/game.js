a6249df74fd852bda13cca8d5afd4f5bddfe4568
Jmactiag <jessie.macatiag@gmail.com>
2017-12-05 18:44:04 -0800

    add code to index html to start the creation of base html layout

---
+++ assets/javascript/game.js
@@ -0,0 +1,9 @@
+alert("hi");
+
+
+
+document.getElementById("gameboard").addEventListener("click", myFunction);
+
+function myFunction() {
+    document.getElementById("gameboard").innerHTML = "YOU CLICKED ME!";
+}
