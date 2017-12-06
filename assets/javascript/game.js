34a856d74e8b5b6357d4e4505c1b1f798c2d81bb
Jmactiag <jessie.macatiag@gmail.com>
2017-12-05 19:24:07 -0800

    add variables to game.js and music files into assets/music

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,9 @@
-alert("hi");
+var songList = ["TOM SAWYER", "RED BARCHETTA", "THE TWILIGHT ZONE", "YYZ", "LIMELIGHT", "MAKING MEMORIES", "CROSSROADS" ];
+var usedLetters=[];
+var numberOfWins=0;
+var guessesRemaining=12;
 
-
+console.log(songList);
 
 document.getElementById("gameboard").addEventListener("click", myFunction);
 
