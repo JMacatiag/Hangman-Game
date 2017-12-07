f4571f05bd382f87bace6015a104023839f65596
Jmactiag <jessie.macatiag@gmail.com>
2017-12-07 15:01:32 -0800

    fix code within javascript and add to index code

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,12 +1,27 @@
-var songList = ["TOM SAWYER", "RED BARCHETTA", "THE TWILIGHT ZONE", "YYZ", "LIMELIGHT", "MAKING MEMORIES", "CROSSROADS" ];
+var songList = ["TOM-SAWYER", "RED-BARCHETTA", "THE-TWILIGHT-ZONE", "YYZ", "LIMELIGHT", "MAKING-MEMORIES", "CROSSROADS" ];
 var usedLetters=[];
 var numberOfWins=0;
 var guessesRemaining=12;
 
-console.log(songList);
+
+
+var wordLetter = songList[0].split("");
+
+	for (i=0; i<wordLetter.length; i++){
+		if (wordLetter[i] === "-"){
+			document.write("- ");
+		}
+		else {
+			document.write("_ ");
+		}
+}
 
 document.getElementById("gameboard").addEventListener("click", myFunction);
 
 function myFunction() {
     document.getElementById("gameboard").innerHTML = "YOU CLICKED ME!";
 }
+
+
+
+
